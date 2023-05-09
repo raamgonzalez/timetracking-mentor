be076e5c03ae069d3aca616d6d7a15064858b20a
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-05-09 14:00:30 -0300

    Add: new data

--- mock/data.js
+++ mock/data.js
@@ -3,6 +3,6 @@ import { v4 as uuidv4} from 'uuid';
 
 export const data = {
 	daily : [2,3,2,2,4,1],
-	weekly : [7,32,10,8,7],
-	monthly : [23,64,12,43,12]
+	weekly : [7,32,10,8,7,12],
+	monthly : [23,64,12,43,12,23]
 }
